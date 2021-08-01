6c1abfc94d2d365cd43a418a7d2c97ad71f6ee45
mingxin400 <89493380@sina.com>
2021-08-01 17:33:34 +0800

    test git

---
+++ src/com/ly/java/TestGit.java
@@ -0,0 +1,14 @@
+package com.ly.java;
+
+import javax.sound.midi.Soundbank;
+
+/**
+ * @author: ly
+ * @date:2021/8/1 - 17:31
+ */
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello idea");
+    }
+}
